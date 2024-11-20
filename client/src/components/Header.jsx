40fd2d8825e5a4e8d083675e514d4b0c152c0816
FrodenZ Labs <frodenzlabs@gmail.com>
2024-11-20 23:04:35 +0300

    Updated padding in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Navbar } from "flowbite-react";
+import { Navbar } from 'flowbite-react';
 
 export const Header = () => {
   return (
-    <Navbar fluid rounded className="bg-white max-w-6xl mx-auto">
+    <Navbar fluid rounded className="bg-white max-w-6xl mx-auto px-5">
       <Navbar.Brand href="">
-        <p className="self-center whitespace-nowrap text-base md:text-4xl text-primary italic font-semibold">FrodenZ <span className="text-secondary">Labs</span></p>
+        <p className="self-center whitespace-nowrap text-xl md:text-4xl text-primary italic font-semibold">
+          FrodenZ <span className="text-secondary">Labs</span>
+        </p>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
@@ -14,10 +16,16 @@ export const Header = () => {
         <Navbar.Link href="#" className="text-base md:text-xl">
           About
         </Navbar.Link>
-        <Navbar.Link href="#" className="text-base md:text-xl">Services</Navbar.Link>
-        <Navbar.Link href="#" className="text-base md:text-xl">Pricing</Navbar.Link>
-        <Navbar.Link href="#" className="text-base md:text-xl">Contact</Navbar.Link>
+        <Navbar.Link href="#" className="text-base md:text-xl">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-base md:text-xl">
+          Pricing
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-base md:text-xl">
+          Contact
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
-}
+  );
+};
